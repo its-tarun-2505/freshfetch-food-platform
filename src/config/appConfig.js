66a2786178919e4343e3649c3e67af36fe808298
Tarun <its.tarun.2505@gmail.com>
2025-08-31 02:43:13 +0530

    remove config file

--- src/config/appConfig.js
+++
@@ -1,9 +0,0 @@
-export const APP_CONFIG = {
-  DEFAULT_RESTAURANT_ID: 'default-restaurant',
-  CLOUDINARY: {
-    CLOUD_NAME: "dst9avpbz",
-    UPLOAD_PRESET: "food delivery app"
-  },
-};
-
-export default APP_CONFIG;
